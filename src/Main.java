3c4895110982d1b0e6d41390e460368d020d8b5d
dyshecova200604-oss <dyshecova.2006.04@gmail.com>
2025-09-12 15:24:25 +0200

    исправила

--- src/Main.java
+++ src/Main.java
@@ -4,59 +4,48 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задача1");
         int clientOS = 0;
-        {
-            System.out.println("Установите приложение для ios по ссылке");
-        }
         if (clientOS == 1) {
-            System.out.println("Установите приложение для Android по ссылке");
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
-        {
-            System.out.println("Задача2");
-        }
+        System.out.println("Задача2");
+        int clientoS = 0; // 0 - iOS, 1 - Android
         int clientDeviceYear = 2014;
-        if (clientDeviceYear >= 2015 && clientOS == 0) {
-            System.out.println("Установите приложение для ios по ссылке");
+        if (clientoS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
-        if (clientDeviceYear >= 2015 && clientOS == 1) {
-            System.out.println("Установите приложение для Android по ссылке");
-        }
-        if (clientDeviceYear < 2015 && clientOS == 0) {
-            System.out.println("Установите облегченную версию приложения для ios по ссылке");
-        }
-        if (clientDeviceYear < 2015 && clientOS == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        {
+
+
             System.out.println("Задача3");
-        }
         int year = 2021;
-        boolean leap = false;
-        if (year %  4 == 0){
-            if (year % 100 == 0) {
-                if (year % 400 == 0)
-                    leap = true;
-                else
-                    leap = false;
-            } else
-                leap = true;
-        }else
-            leap = false;
-
-
-        if (leap)
-            System.out.println( "год является високосным ");
-        else
-            System.out.println(" год не является високосным ");
-        {
-            System.out.println("Задача4");
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+        } else {
+            System.out.println("Год должен быть больше 1584");
         }
+            System.out.println("Задача4");
         int deliveryDistance= 95;
         int days;
         if (deliveryDistance<=20){
             days= 1;
-        }else if (deliveryDistance>20&&deliveryDistance<=60){
+        }else if (deliveryDistance<=60){
             days =2;
-        }else if (deliveryDistance>60&&deliveryDistance<=100) {
+        }else if (deliveryDistance<=100) {
             days = 3;
         }else {
             days = -1;
@@ -66,43 +55,30 @@ public class Main {
         }else {
             System.out.println("Доставки нет");
         }
-
-        System.out.println("Задача5");
+            System.out.println("Задача5");
         int monthNumber= 12;
-        switch (monthNumber){
+        switch (monthNumber) {
             case 12:
             case 1:
             case 2:
-                System.out.
-
-                        println("Зима");
+                System.out.println("Зима");
                 break;
             case 3:
             case 4:
             case 5:
-                System.out.
-
-                        println("Весна");
+                System.out.println("Весна");
                 break;
             case 6:
             case 7:
             case 8:
-                System.out.
-
-                        println("Лето");
+                System.out.println("Лето");
                 break;
             case 9:
             case 10:
             case 11:
-                System.out.
-
-                        println("Осень");
+                System.out.println("Осень");
                 break;
             default:
-                System.out.
+                System.out.println("Неверный номер месяца!");
 
-                        println("Неверный номер месяца!");
-        }
-    }
-}
-
+        }}}
